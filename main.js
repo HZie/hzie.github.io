e4f2ce861a08391f5f4e953d61c310dc647c1a8e
root <jyjy9734@gmail.com>
2021-11-02 16:20:13 +0900

    js: home contact button

--- main.js
+++ main.js
@@ -18,18 +18,26 @@ document.addEventListener('scroll', () => {
 
 });
 
+// function: scrolling to selector
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    // console.log("clicked");
     const target = event.target;
     const link = target.dataset.link;
-    if(link == null){
+    if(link == null)
         return;
-    }
-    console.log(event.target.dataset.link);
 
+    scrollIntoView(link);
     
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-})
+});
+
+// Handle click on contact button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
