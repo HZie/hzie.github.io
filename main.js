ad64f1c33e373d2f571e904220766805d6cf753f
root <jyjy9734@gmail.com>
2021-11-02 15:02:44 +0900

    js: navbar

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Make navbar transparent when it is on top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+
+    if(window.scrollY >= navbarHeight){
+        console.log("window.scrollY > navbarHeight");
+        navbar.classList.add('navbar--dark');
+    }
+    else{
+        navbar.classList.remove('navbar--dark');
+    }
+
+});
+
