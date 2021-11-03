47530dc67895b8fa3a57932367855f859acf4cfa
root <boss.hz734@gmail.com>
2021-11-03 13:45:35 +0900

    js: project filtering

--- main.js
+++ main.js
@@ -72,3 +72,31 @@ const handleArrowUp = ()=>{
 };
 
 arrowUpBtn.addEventListener('click', handleArrowUp);
+
+// filter projects
+const categoryBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+categoryBtnContainer.addEventListener('click', (event) => {
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+
+    setTimeout(() => {        
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }
+            else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+
+});
