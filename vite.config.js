f7c1079e14b7657e9234b1018e45d73a690cbf22
HZie <boss.hz734@gmail.com>
2025-07-27 00:25:54 +0900

    replaced to React+Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://jiyeon.han347.com/", // <-- 예: '/portfolio-react-new/'
+  base: "https://hzie.github.io/", // <-- 예: '/portfolio-react-new/'
   plugins: [react()],
   build: {
     outDir: "dist",
