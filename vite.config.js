7a351f128f8b7a24c87c2d8d77e5455f81750625
HZie <boss.hz734@gmail.com>
2025-07-27 00:31:52 +0900

    replaced to React+Vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 // vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   base: "./", // <-- 절대경로(‘/repo/’)가 아니라 "./" 로
   plugins: [react()],
